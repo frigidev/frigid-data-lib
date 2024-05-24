60ab774c8d704547dc8ebbf261d1b0f2746f1209
Orestes Lucas <oresteslucas@gmail.com>
2024-05-23 23:57:46 -0300

    Update SortImplementation.java

--- src/frigid/data/lib/classes/SortImplementation.java
+++ src/frigid/data/lib/classes/SortImplementation.java
@@ -7,35 +7,39 @@ public class SortImplementation implements Sort{
 
 	/*
 	 * This method is a simple sort algorithm, called bubbleSort, that works fine for sorting a small list of elements, with a 
-	 * time complexity of O(n^2) in the worst case.
+	 * space complexity of O(n^2) in the worst case.
 	 */
 	@Override
 	public LinkedList sort(LinkedList list) {
 		Node current;
 		int aux;
 		
-		while(true) {
-			current = list.getFirst();
-			boolean swapped = false;
-			while(current != null) {
-				Node next = current.getNext();
-				while(next != null) {
-					if(current.getValue() > next.getValue()) {
-						swapped = true;
-						aux = current.getValue();
-						current.setValue(next.getValue());
-						next.setValue(aux);
+		try {
+			if(list.isEmpty()) {
+				throw new ExceptionHandler("The list is empty");
+			}
+			while(true) {
+				current = list.getFirst();
+				boolean swapped = false;
+				while(current != null) {
+					Node next = current.getNext();
+					while(next != null) {
+						if(current.getValue() > next.getValue()) {
+							swapped = true;
+							aux = current.getValue();
+							current.setValue(next.getValue());
+							next.setValue(aux);
+						}
+						next = next.getNext();
 					}
-					next = next.getNext();
+					current = current.getNext();
 				}
-				current = current.getNext();
+				if(!swapped) {
+					break;
+				}
 			}
-			if(!swapped) {
-				break;
-			}
-		}
-		if(list.isEmpty()) {
-			ExceptionHandler.emptyList();
+		} catch(ExceptionHandler e) {
+			System.out.println(e.getMessage());
 		}
 		return list;
 	}
