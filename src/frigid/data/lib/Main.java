58a42e54c978e0f521fcdc4918cee4e76090d6e3
Orestes Lucas <oresteslucas@gmail.com>
2024-05-23 23:56:19 -0300

    Update Main.java

--- src/frigid/data/lib/Main.java
+++ src/frigid/data/lib/Main.java
@@ -5,7 +5,6 @@ import javax.swing.JOptionPane;
 import frigid.data.lib.classes.LinearSearch;
 import frigid.data.lib.classes.LinkedList;
 import frigid.data.lib.classes.SortImplementation;
-import frigid.data.lib.exceptionhandler.ExceptionHandler;
 
 public class Main {
 	public static void main(String[] args) {
@@ -14,11 +13,11 @@ public class Main {
 		
 		while(true) {
 			int op = Integer.parseInt(JOptionPane.showInputDialog("Choose one operation of the list:\n1 - add number\n2 - remove number\n3 - show list\n"
-					+ "4 - show first and last\n5 - size of the list\n6 - get a number of the list by position\n7 - more options\n8 - exit"));	
+					+ "4 - show first number\n5 - show last number\n6 - size of the list\n7 - get a number of the list by position\n8 - more options\n9 - exit"));	
 		
 			switch(op) {
 				case 1:
-					int value = Integer.parseInt(JOptionPane.showInputDialog("Type a number."));
+					int value = Integer.parseInt(JOptionPane.showInputDialog("Enter a number."));
 					int position = Integer.parseInt(JOptionPane.showInputDialog("Do you want to insert your number in which position of the list?\n"
 							+ "1 - begin of the list\n2 - middle of the list\n3 - end of the list"));
 					if(position == 1) {
@@ -31,7 +30,7 @@ public class Main {
 					}
 					continue;
 				case 2:
-					int remove = Integer.parseInt(JOptionPane.showInputDialog("Type a number to remove."));
+					int remove = Integer.parseInt(JOptionPane.showInputDialog("Type a number to remove"));
 					list.remove(remove);
 					continue;
 				case 3:
@@ -39,28 +38,30 @@ public class Main {
 					System.out.println();
 					continue;
 				case 4:
-					list.firstNode();
-					list.lastNode();
+					JOptionPane.showMessageDialog(null, "The first node of the list is: "  +  list.firstNode());
 					continue;
 				case 5:
-					JOptionPane.showMessageDialog(null, "The size of the list is: " + list.countNodes());
+					JOptionPane.showMessageDialog(null, "The last node of the list is: "  +  list.lastNode());
 					continue;
 				case 6:
-					int positionNode = Integer.parseInt(JOptionPane.showInputDialog("Type a position that you want to know the number."));
-					if(!list.isEmpty()) {
-						JOptionPane.showMessageDialog(null, "The number of this position is:");
-					}
-					JOptionPane.showMessageDialog(null, list.getNode(positionNode));
+					JOptionPane.showMessageDialog(null, "The size of the list is: " + list.countNodes());
 					continue;
 				case 7:
+					int positionNode = Integer.parseInt(JOptionPane.showInputDialog("Enter a position that you want to know the number."));
+					if(!list.isEmpty()) {
+						JOptionPane.showMessageDialog(null, "The number of this position will be displayed on the console if exists");
+					}
+					System.out.println(list.getNode(positionNode));
+					continue;
+				case 8:
 					int moreOptions = Integer.parseInt(JOptionPane.showInputDialog("1 - to sort the numbers\n2 - to search a number\n3 - to show the pairs or odd numbers\n4 - to " +
-							"show the sum of all numbers\n5 - back to main menu"));
+							"show the sum of all numbers\n5 - to reset the list\n6 - back to main menu"));
 					switch(moreOptions) {
 						case 1: 
 							SortImplementation sortList = new SortImplementation();
 							sortList.sort(list);
 							if(!list.isEmpty()) {
-								JOptionPane.showMessageDialog(null, "The numbers now are sorted. The current list is:");
+								JOptionPane.showMessageDialog(null, "The numbers now are sorted. The current list is displayed on the console");
 								list.showList();
 							}
 							continue;
@@ -77,20 +78,17 @@ public class Main {
 							}
 							continue;
 						case 4:
-							if(!list.isEmpty()) {
-								JOptionPane.showMessageDialog(null, "The sum of all numbers is:");
-								JOptionPane.showMessageDialog(null, list.sumNodes());
-							}else {
-								ExceptionHandler.emptyList();
-							}
+							JOptionPane.showMessageDialog(null, "The sum of all numbers is: " + list.sumNodes());
 							continue;
 						case 5:
+							list.destroy();
+						case 6:
 							continue;
 					}
-				case 8:
+				case 9:
 					JOptionPane.showMessageDialog(null, "End of execution");
 					return;
 			}
-		}		
+		}
 	}
 }
