469e4f6751c4babfbf9f478871d8ad5728ac499d
Orestes Lucas <oresteslucas@gmail.com>
2024-05-23 23:59:08 -0300

    Update LinearSearch.java

--- src/frigid/data/lib/classes/LinearSearch.java
+++ src/frigid/data/lib/classes/LinearSearch.java
@@ -1,11 +1,10 @@
 package frigid.data.lib.classes;
 
 import javax.swing.JOptionPane;
-
 import frigid.data.lib.interfaces.Search;
 
 public class LinearSearch implements Search{
-
+	
 	/*
 	 * This is a method to make a linear search of an element in a LinkedList. It works fine for a small LinkedList.
 	 */
@@ -18,6 +17,10 @@ public class LinearSearch implements Search{
 		
 		boolean found = false;
 		int size = list.countNodes();
+		
+		if(size == 0) {
+			return;
+		}
 		
 		for(int i=0; i<size; i++) {
 			if(list.getFirst().getValue() == searched || pointer.getValue() == searched) {
